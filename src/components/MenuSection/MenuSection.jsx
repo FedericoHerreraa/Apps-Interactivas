ea93a1a66b9db4d692b9fc961995363d4b735d1a
FedericoHerreraa <fede.juan.herrera@gmail.com>
2025-06-21 18:28:00 -0300

    adding fetching of meals

--- src/components/MenuSection/MenuSection.jsx
+++ src/components/MenuSection/MenuSection.jsx
@@ -1,14 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Menu.css';
 import { menuData } from '../../data/data';
 import { MenuItem } from './MenuItem';
 
 function MenuSection() {
   const [selectedImg, setSelectedImg] = useState(null);
+  const [menuDataFetched, setMenuDataFetched] = useState(null);
 
   const handleClickOutside = () => {
     setSelectedImg(null);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const responseLogin = await fetch(`http://localhost:3000/api/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: "fede.juan.herrera@gmail.com",
+          password: "123456"
+        })
+      })
+
+      const dataLogin = await responseLogin.json()
+
+      if (!responseLogin.ok) {
+        throw new Error(dataLogin.message || "Error al iniciar sesión")
+      }
+
+      const response = await fetch(`http://localhost:3000/api/dishes`, {
+        headers: {
+          Authorization: `Bearer ${dataLogin.token}`
+        }
+      })
+
+      const dataDishes = await response.json()
+      console.log(dataDishes)
+      setMenuDataFetched(dataDishes)
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <div id="menuSection" className="menu-container">
@@ -16,10 +50,10 @@ function MenuSection() {
       <p>Descubre nuestras deliciosas opciones</p>
 
       {menuData.map((section, index) => (
-        <MenuItem key={index} section={section} setSelectedImg={setSelectedImg}/>
+        <MenuItem key={index} section={section} setSelectedImg={setSelectedImg} />
       ))}
 
-      
+
       {selectedImg && (
         <div className="image-overlay" onClick={handleClickOutside}>
           <img src={selectedImg} alt="ampliada" />
