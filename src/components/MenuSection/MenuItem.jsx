315c7fc3ec5cd2a9e8e2153adc62c9694333e8a7
FedericoHerreraa <fede.juan.herrera@gmail.com>
2025-07-01 15:53:09 -0300

    connecting backend with menu

--- src/components/MenuSection/MenuItem.jsx
+++ src/components/MenuSection/MenuItem.jsx
@@ -1,58 +1,27 @@
-
 import { Link } from "react-router-dom"
 
-export const MenuItem = ({ section, setSelectedImg }) => {
-    return (
-        <div className="menu-section">
-          <h2>{section.category}</h2>
-          {section.subcategories ? (
-            <div className="subcategories" style={{ textAlign: 'left' }}>
-              {section.subcategories.map((subcategory, subIndex) => (
-                <div key={subIndex} className="menu-subcategory">
-                  <h3 >{subcategory.name}</h3>
-                  {subcategory.items.map((item, i) => (
-                    <Link to={`/plato/${item.id}`} key={i} className="menu-item">
-                      <div>
-                        <div style={{ marginBottom: '10px', textAlign: 'start' }}>
-                          <h3>{item.name}</h3>
-                          <p>{item.description}</p>
-                        </div>
-                        <p><strong>Ingredientes:</strong> {item.ingredientes}</p>
-                        <p><strong>Alérgenos:</strong> {item.alergenos}</p>
-                        <span className="price">{item.price}</span>
-                      </div>
-                      <img
-                        src={item.img}
-                        alt={item.name}
-                        className="menu-item-img"
-                        onClick={() => setSelectedImg(item.img)}
-                      />
-                    </Link>
-                  ))}
-                </div>
-              ))}
-            </div>
-          ) : (
-            section.items.map((item, i) => (
-              <Link to={`/plato/${item.id}`}  key={i} className="menu-item">
-                <div>
-                  <div style={{ marginBottom: '10px', textAlign: 'start' }}>
-                    <h3>{item.name}</h3>
-                    <p>{item.description}</p>
-                  </div>
-                  <p><strong>Ingredientes:</strong> {item.ingredientes}</p>
-                  <p><strong>Alérgenos:</strong> {item.alergenos}</p>
-                  <span className="price">{item.price}</span>
-                </div>
-                <img
-                  src={item.img}
-                  alt={item.name}
-                  className="menu-item-img"
-                  onClick={() => setSelectedImg(item.img)}
-                />
-              </Link>
-            ))
-          )}
+
+
+export const MenuItem = ({ item, setSelectedImg }) => {
+  console.log(item)
+
+  return (
+    <Link to={`/plato/${item.id}`} className="menu-item">
+      <div>
+        <div style={{ marginBottom: '10px', textAlign: 'start' }}>
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
         </div>
-    )
+        <p><strong>Ingredientes:</strong> {item.ingredientes}</p>
+        <p><strong>Alérgenos:</strong> {item.alergenos}</p>
+        <span className="price">${item.price}</span>
+      </div>
+      <img
+        src={item.image}
+        alt={item.name}
+        className="menu-item-img"
+        onClick={() => setSelectedImg(item.image)}
+      />
+    </Link>
+  )
 }
--- src/components/MenuSection/MenuSection.jsx
+++ src/components/MenuSection/MenuSection.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './Menu.css';
-import { menuData } from '../../data/data';
 import { MenuItem } from './MenuItem';
 
 function MenuSection() {
   const [selectedImg, setSelectedImg] = useState(null);
   const [menuDataFetched, setMenuDataFetched] = useState(null);
 
-  // Variables de entorno
-  const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+  const API_URL = 'http://localhost:3000';
   const USER_EMAIL = import.meta.env.VITE_USER_EMAIL || 'fede.juan.herrera@gmail.com';
   const USER_PASSWORD = import.meta.env.VITE_USER_PASSWORD || '123456';
 
@@ -43,21 +41,66 @@ function MenuSection() {
 
       const dataDishes = await response.json()
       setMenuDataFetched(dataDishes)
-      console.log(menuDataFetched)
+
+      if (!response.ok) {
+        throw new Error(dataDishes.message || "Error al obtener los platos")
+      }
     }
 
     fetchData()
   }, [])
+
+  if (!menuDataFetched) {
+    return <div>Cargando...</div>
+  }
+
+  function groupByCategoryAndSubcategory(products) {
+    const grouped = {};
+
+    products.forEach(product => {
+      const category = product.category || "Sin categoría";
+      const subcategory = product.subcategory || "";
+
+      if (!grouped[category]) grouped[category] = {};
+      if (!grouped[category][subcategory]) grouped[category][subcategory] = [];
+
+      grouped[category][subcategory].push(product);
+    });
+
+    return grouped;
+  }
+
+  const groupedMenu = groupByCategoryAndSubcategory(menuDataFetched);
 
   return (
     <div id="menuSection" className="menu-container">
       <h1 style={{ alignItems: 'center' }} >Menú</h1>
       <p>Descubre nuestras deliciosas opciones</p>
 
-      {menuData.map((section, index) => (
-        <MenuItem key={index} section={section} setSelectedImg={setSelectedImg} />
+      {Object.entries(groupedMenu).map(([category, subcats]) => (
+        <div key={category}>
+          <h2 style={{ textAlign: 'left', marginTop: '100px' }}>{category.replace("_", " ").toLocaleUpperCase()}</h2>
+          {Object.entries(subcats).map(([subcategory, items]) => (
+            <div key={subcategory}>
+              {subcategory && (
+                <h3
+                  style={{
+                    textAlign: 'left',
+                    color: '#cccccc',
+                    marginTop: 0,
+                    marginBottom: '10px'
+                  }}
+                >
+                  {subcategory.replace("_", " ").toLocaleUpperCase()}
+                </h3>
+              )}
+              {items.map(item => (
+                <MenuItem key={item._id} item={item} setSelectedImg={setSelectedImg} />
+              ))}
+            </div>
+          ))}
+        </div>
       ))}
-
 
       {selectedImg && (
         <div className="image-overlay" onClick={handleClickOutside}>
